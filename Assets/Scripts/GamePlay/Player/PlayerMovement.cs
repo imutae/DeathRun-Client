8f18a85ddb0b813fa9176eeb6108ea53ac11109c
imutae <imutae.lab@gmail.com>
2026-04-28 21:18:13 +0900

    맵 변경으로 인한 기능 수정

--- Assets/Scripts/GamePlay/Player/PlayerMovement.cs
+++ Assets/Scripts/GamePlay/Player/PlayerMovement.cs
@@ -1,10 +1,9 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D _rigidbody2D = null;
-    private bool _isWall = false;
 
     private void Awake()
     {
@@ -13,32 +12,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnMove(InputValue input)
     {
-        if(_isWall)
-        {
-            return;
-        }
-
         Vector2 moveInput = input.Get<Vector2>();
         _rigidbody2D.linearVelocityX = moveInput.x * 5f;
-    }
-
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        if(collision.gameObject.CompareTag("Ground"))
-        {
-            ContactPoint2D[] contactPoints = collision.contacts;
-
-            foreach(ContactPoint2D contact in contactPoints)
-            {
-                if(contact.normal.y < 0.5f && (contact.normal.x > 0.5f || contact.normal.x < -0.5f))
-                {
-                    _isWall = true;
-                    _rigidbody2D.AddForceY(-1f, ForceMode2D.Impulse);
-                    return;
-                }
-            }
-
-            _isWall = false;
-        }
     }
 }
