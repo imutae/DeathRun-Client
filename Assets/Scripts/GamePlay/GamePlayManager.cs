edf48ea730ed0c8e4ce53bed05c78c5d78194250
imutae <imutae.lab@gmail.com>
2026-04-28 15:19:00 +0900

    캐릭터 생성 및 삭제 개발 완료

--- Assets/Scripts/GamePlay/GamePlayManager.cs
+++ Assets/Scripts/GamePlay/GamePlayManager.cs
@@ -1,19 +1,60 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GamePlayManager : MonoBehaviour
 {
+    private Dictionary<long, GameObject> _otherPlayers = new Dictionary<long, GameObject>();
+
     private void Awake()
     {
-        
-    }
+        NetworkManager.Instance.OnOtherPlayerJoined += JoinOtherUser;
+        NetworkManager.Instance.OnOtherPlayerLeft += ExitOtherUser;
+        NetworkManager.Instance.OnMoveReceived += OtherPlayerMoveRecive;
+
+        PoolManager.Instance.PreloadAsync("OtherCharacter", 7).Wait();
+    }    
 
     private void JoinOtherUser(long userId)
     {
-        
+        if (userId == PlayerManager.Instance.PlayerName)
+        {
+            return;
+        }
+
+        GameObject otherCharacter = PoolManager.Instance.Spawn("OtherCharacter", Vector3.zero, Quaternion.identity);
+        _otherPlayers.Add(userId, otherCharacter);
+    }
+
+    private void ExitOtherUser(long userId)
+    {
+        if (userId == PlayerManager.Instance.PlayerName)
+        {
+            return;
+        }
+
+        if (_otherPlayers.TryGetValue(userId, out GameObject otherCharacter))
+        {
+            PoolManager.Instance.Despawn(otherCharacter);
+            _otherPlayers.Remove(userId);
+        }
+    }
+
+    private void OtherPlayerMoveRecive(SMovePacket movePacket)
+    {
+        if (_otherPlayers.TryGetValue(movePacket.SessionId, out GameObject otherCharacter))
+        {
+            Vector2 newPosition = new Vector2(movePacket.X, movePacket.Y);
+            otherCharacter.transform.position = newPosition;
+        }
     }
 
     public void ExitGame()
     {
+        // Callback 초기화
+        NetworkManager.Instance.OnOtherPlayerJoined -= JoinOtherUser;
+        NetworkManager.Instance.OnOtherPlayerLeft -= ExitOtherUser;
+
+        // Scene 전환
         NetworkManager.Instance.LeaveRoom();
         SceneLoadManager.Instance.LoadLobbyScene();
     }
